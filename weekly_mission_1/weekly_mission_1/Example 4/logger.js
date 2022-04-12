6cf9e2538014d4397250c221804188e382117d15
Hector <99096517+HectorMancillaS@users.noreply.github.com>
2022-04-11 20:42:53 -0500

    Actualizacion

---
+++ weekly_mission_1/weekly_mission_1/Example 4/logger.js
@@ -0,0 +1,14 @@
+class Logger {
+    constructor(name) {
+    }
+  
+    info (message) {
+      console.log(`[Objeto con nombre: ${this.name}] info: ${message}`)
+    }
+  
+    verbose (message) {
+      console.log(`[Objeto con nombre: ${this.name}] verbose: ${message}`)
+    }
+  }
+  
+  module.exports = Logger
