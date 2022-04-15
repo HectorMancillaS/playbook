f49df9710de8ef1d017f197396104da800eaa816
Hector <99096517+HectorMancillaS@users.noreply.github.com>
2022-04-14 21:45:19 -0500

    Live 14/4

---
+++ weekly_mission_2/exercises on live/example2.js
@@ -0,0 +1,52 @@
+console.loge("POO")
+
+// Objeto
+
+
+    mission: "Node JS"
+    age: "12"
+    color: "pink"
+}
+
+// Crear una clase
+/*
+1. Como crear una clase
+2. Como intanciar un objeto de una clase
+3. Instanciar
+class Ajoplonauta{
+    constructor(name, mission, age, color) {
+        this.name = name
+        this.mission = mission
+        this.age = age
+        this.color = color
+    }
+
+    sayHello() {
+        return '${this.name} is saying Heloo!' Hello"
+    }
+}
+
+class Pokemon{
+    constructor(name) {
+        this.name = name
+    }
+}
+
+const pikachu = new Pokemon("pikachu")
+console.log()
+// Objeto de JS
+const woopa_obj_js = {}
+console.log(Ajolonauta)
+
+const woopa = new Ajolonauta("Woopa", "Node JS",) // Instanciar un objeto
+const wooper = new Ajolonauta()
+const woopy = new Ajolonauta()
+
+
+console.log(woopa_obj_js)
+
+console.log(woopa)
+console.log(wooper)
+console.log(woopy) // Instanciar un objeto
+
+console.log(woopa_obj_js)
